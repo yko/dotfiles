d890f5eb77bbd22373bdd6686dffe40370fea15d
yko <ykorshak@gmail.com>
2011-08-10 15:36:38 +0300

    add module-starter support

---
+++ .module-starter/PBP/Module.pm
@@ -0,0 +1,36 @@
+package <MODULE NAME>;
+
+use warnings;
+use strict;
+require Carp;
+
+our $VERSION = 0.01;
+
+1;
+
+__END__
+
+=head1 NAME
+
+<MODULE NAME> - [One line description of module's purpose here]
+
+
+=head1 SYNOPSIS
+
+    use <MODULE NAME>;
+
+
+=head1 DESCRIPTION
+
+<MODULE NAME>
+
+=head1 ATTRIBUTES
+
+L<<MODULE NAME>> implements following attributes:
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (C) <YEAR>, <AUTHOR>.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
